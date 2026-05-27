31165411a8c51c9931444822da4c3754df2a49a0
22012871-Triston <22012871@myrp.edu.sg>
2026-05-27 11:30:17 +0800

    Fix Vercel Blank page

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import.meta.env.VITE_FIREBASE_API_KEY
 
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
