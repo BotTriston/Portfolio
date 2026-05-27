b744d1bfd0eca36aab9e699c5e002a13716ad954
22012871-Triston <22012871@myrp.edu.sg>
2026-05-27 11:52:19 +0800

    Fix Github pages development

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import.meta.env.VITE_FIREBASE_API_KEY
 
 const firebaseConfig = {
   apiKey: "YOUR_API_KEY",
